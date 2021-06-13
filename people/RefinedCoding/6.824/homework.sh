7722d1ea0fa33bf79b783f286cd8dc1807145cf0
RefinedCoding <RefinedCoding@gmail.com>
2021-06-13 00:50:11 -0700

    upload homework 0613

--- people/RefinedCoding/6.824/homework.sh
+++ people/RefinedCoding/6.824/homework.sh
@@ -2,6 +2,7 @@ cp src/mr/rpc.go ../../../homework/0528/RefinedCoding-rpc.go
 cp src/mr/coordinator.go ../../../homework/0529/RefinedCoding-coordinator.go
 cp src/mr/worker.go ../../../homework/0530/RefinedCoding-worker.go
 
+cp src/raft/raft.go ../../../homework/0613/RefinedCoding-raft.go
 
 
 
